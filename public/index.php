d08c5e67743f46e452b815f2c2529f846e807a32
ETI-activestudents <eti.activestudents@gmail.com>
2022-11-21 16:31:57 +0200

    21.11.2022 add php

--- public/index.php
+++ public/index.php
@@ -1,4 +1,4 @@
 <?php
-
-include 'vendor/autoload.php';
-include 'public/pages/main.php';
+    $root = dirname(__DIR__);
+    include $root.'/vendor/autoload.php';
+    include $root.'/public/pages/main.php';
